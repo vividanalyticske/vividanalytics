75969dba859cf0dd576392dc3234330decc9ebf6
geofrey <devongeoffreymaina@gmail.com>
2025-07-15 08:39:56 +0300

    added subdomain to nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 const nextConfig = {
   // Your Next.js config here
   images: {
-    domains: ['localhost', 'vividanalytics.co'],
+    domains: ['localhost', 'vividanalytics.co', 'www.vividanalytics.co'],
   },
 }
 
